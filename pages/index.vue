f86f62ad725e1a4449bbdc405afe36ce06a4c290
sk0540 <suzaki9607@gmail.com>
2022-03-03 15:49:45 +0900

    Upload Vue code

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="wrapper">
+        <header>
+            <div id="title">
+                <h1>sk0540</h1>
+                <p>sk0540のWebサイト</p>
+            </div>
+            <nav>
+    
+                <a class="selected" href=#about>Top</a>
+                <a href=#works>Works</a>
+                <a href="blog/" >Blog</a>
+            </nav>
+        </header>
+        <div id="content">
+            <main >
+                <h1 class="headline" id="about">About<span class="subtext">このサイトについて</span></h1>
+                sk0540です。このwebサイトにWebデザインとプログラミングの制作物を載せていく予定です。
+
+                <h1 class="headline" id="works">Works<span class="subtext">制作作品</span></h1>
+    
+            </main>
+            <aside>
+                <p>サイドメニュー予定地ですサイドメニュー予定地ですサイドメニュー予定地です</p>
+            </aside>
+        </div>
+      <footer><div class="copyright">©2021-2022 sk0540</div></footer>
+    </div>
+</template>
+
+<script>
+</script>
+
+
+
