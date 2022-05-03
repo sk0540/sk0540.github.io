03dd18574e5567604d6e999ff5e980dbdffcff21
sk0540 <sk0540@users.noreply.github.com>
2022-05-03 06:28:14 +0000

    deploy: c06de5b932fe1b48cb4b959e52ac0a9fcac01544

--- _nuxt/static/1646879153/blog/8i7zq5_kdg/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646879153",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F8i7zq5_kdg",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
